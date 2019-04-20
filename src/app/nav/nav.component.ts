b25f9ebdd89274542d8356c1392e2b56f34e3b76
james3014 <grantj3014@gmail.com>
2019-04-20 08:11:01 +0100

    Error intepreter added

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -20,7 +20,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log('Logged In Successfully');
     }, error => {
-      console.log('Failed To Login');
+      console.log(error);
     });
   }
 
