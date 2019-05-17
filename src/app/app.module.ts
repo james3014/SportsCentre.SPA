9760f7d70cc8ab96bba717dc2064202a58e8b22b
james3014 <grantj3014@gmail.com>
2019-05-17 06:16:14 +0100

    Updated booking component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BsDropdownModule, BsDatepickerModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
 
@@ -66,6 +66,8 @@ export function tokenGetter() {
       BsDatepickerModule.forRoot(),
       BsDropdownModule.forRoot(),
       RouterModule.forRoot(appRoutes),
+      FormsModule,
+      ReactiveFormsModule
    ],
    providers: [
       AuthService,
--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -1,10 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Self, Optional } from '@angular/core';
 import { DataService } from '../../_services/data.service';
-import { Booking } from '../../_models/booking';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { BsDatepickerConfig } from 'ngx-bootstrap';
 import { AuthService } from 'src/app/_services/auth.service';
-
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+  NgControl
+} from '@angular/forms';
 
 @Component({
   selector: 'app-booking',
@@ -15,7 +20,30 @@ export class BookingComponent implements OnInit {
   model: any = {};
   bsConfig: Partial<BsDatepickerConfig>;
 
-  constructor(private dataService: DataService, private alertify: AlertifyService, private authService: AuthService) { }
+  form = new FormGroup({
+    bookingTime: new FormControl(null, Validators.required)
+  });
+
+  options = [
+    { id: 1, label: '08:00 - 09:00' },
+    { id: 2, label: '09:00 - 10:00' },
+    { id: 3, label: '10:00 - 11:00' },
+    { id: 4, label: '11:00 - 12:00' },
+    { id: 5, label: '12:00 - 13:00' },
+    { id: 6, label: '13:00 - 14:00' },
+    { id: 7, label: '14:00 - 15:00' },
+    { id: 8, label: '15:00 - 16:00' },
+    { id: 9, label: '16:00 - 17:00' },
+    { id: 10, label: '17:00 - 18:00' },
+    { id: 11, label: '18:00 - 19:00' },
+    { id: 12, label: '19:00 - 20:00' },
+  ];
+
+  constructor(
+    private dataService: DataService,
+    private alertify: AlertifyService,
+    private authService: AuthService,
+  ) {}
 
   ngOnInit() {
     this.bsConfig = {
@@ -25,11 +53,13 @@ export class BookingComponent implements OnInit {
 
   createBooking() {
     const email = this.authService.currentUser.email;
-    this.dataService.createBooking(this.model, email).subscribe(next => {
-      this.alertify.success('Booking Created');
-    }, error => {
-      this.alertify.error('Booking Failed');
-    });
+    this.dataService.createBooking(this.model, email).subscribe(
+      next => {
+        this.alertify.success('Booking Created');
+      },
+      error => {
+        this.alertify.error('Booking Failed');
+      }
+    );
   }
-
 }
